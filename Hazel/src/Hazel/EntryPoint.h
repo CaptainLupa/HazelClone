84f185af26993ba9a9faae2ee6efe3478e66d9e1
Jens Eckert <capt.lupa@gmail.com>
2020-12-06 16:38:05 -0500

    Logging Macros

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,8 +6,9 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
 	Hazel::Log::Init();
-	Hazel::Log::GetCoreLogger()->warn("Initialized Log!");
-	Hazel::Log::GetCoreLogger()->info("Hello!");
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! var={0}", a);
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
