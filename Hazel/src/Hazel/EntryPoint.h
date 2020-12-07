5cc6a24a3e78620d3be1c8b696baaf57922b50eb
Jens Eckert <capt.lupa@gmail.com>
2020-12-06 21:17:16 -0500

    Fixed Log Macros
    Changed HZ_FATAL and HZ_CORE_FATAL to HZ_CRITICAL and HZ_CORE_CRITCAl and also changed coresponding logging functions.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -9,6 +9,9 @@ int main(int argc, char** argv) {
 	HZ_CORE_WARN("Initialized Log!");
 	int a = 5;
 	HZ_INFO("Hello! var={0}", a);
+	HZ_ERROR("OH NOOOOOOOO");
+	HZ_CORE_TRACE("ERRROROROROROROR");
+	HZ_CRITICAL("GOD SEX");
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
